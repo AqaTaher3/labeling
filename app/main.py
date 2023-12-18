56779a9d4cfd6a5fbead668038c0fab5aafd4443
Taher <aqa.taher3@gmail.com>
2023-12-18 13:11:27 +0330

    adding env

--- app/main.py
+++ app/main.py
@@ -105,7 +105,7 @@ def make_color_format(input_color):
     return ((int(RGB[0]), int(RGB[1]), int(RGB[2])))
 
 
-string = input('writw          :')
+string = os.environ.get()
 
 input = string.split("+")
 
@@ -118,6 +118,3 @@ if len(input) == 4:
     main(Json_file, Video_file, pixalation, make_color_format(input[3]))
 else:
     main(Json_file, Video_file, pixalation)
-
-
-# # aqua.json+tom.mp4+blur+(256, 256, 0)
